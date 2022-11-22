01beabdc81fc45a01859947705e5f02cdf65857a
nermin0327 <114156272+nermin0327@users.noreply.github.com>
2022-11-22 15:14:56 +0400

    haribo

---
+++ src/Compenent/Price.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+ const Price=()=>{
+    return(
+        <>
+        <p>120 MANAT</p>
+        </>
+    )
+ }
+
+ export default Price
---
+++ src/Compenent/Product.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Price from "./price";
+import ProductName from "./productName";
+
+ const Product=()=>{
+    return(
+        <><ProductName/>
+        <Price/>
+        </>
+        
+    )
+        
+ }
+
+ export default Product
